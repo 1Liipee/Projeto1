efabe2680f9b98dfebde33ec9fa58b4d07ac12e8
1Liipee <Felipe_45816@hotmail.com>
2019-04-12 18:44:03 -0300

    Alteração do banco de dados local para online

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //Conexão do Banco de Dados utilizando Mongoose
-mongoose.connect('mongodb://localhost:27017/EmployeeDB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://Liipee:Flamengo962@projetoaldo-wlwzq.mongodb.net/', {dbName: 'EmployeeDB'}, (err) => {
     if (!err) { console.log('Conexão ao Banco de Dados MongoDB efetuada com sucesso.') }
     else { console.log('Falha de conexão ao Banco de Dados : ' + err) }
 });
